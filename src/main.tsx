cef310809a230eb73053fcc75be15908fea63fd7
urquico <44643247+urquico@users.noreply.github.com>
2025-06-21 09:47:44 +0800

    fix: missing dummy for tables (#29)
    Co-authored-by: kurturquico <kurt.urquico@simplevia.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,10 @@
-import '@mantine/core/styles.css';
 import '@mantine/charts/styles.css';
+import '@mantine/core/styles.css';
 import '@mantine/dates/styles.css';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-
 
 import App from './App.tsx';
 import './assets/fonts/fonts.css';
 import './index.css';
 
-
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById('root')!).render(<App />);
